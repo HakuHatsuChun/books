b21860bd1b9cd4d085543aaefed9625114a6228e
ほりとーま <116673592+HakuHatsuChun@users.noreply.github.com>
2022-12-16 06:51:52 +0900

    Add files via upload

---
+++ PHP_kadai2.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost
+-- 生成日時: 2022 年 12 月 15 日 22:34
+-- サーバのバージョン： 10.4.21-MariaDB
+-- PHP のバージョン: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `PHP_kadai2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE `gs_bm_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `source` text NOT NULL,
+  `comment` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `gs_bm_table`
+--
+
+INSERT INTO `gs_bm_table` (`id`, `name`, `source`, `comment`, `date`) VALUES
+(7, 'かいけつゾロリ', 'amazon.co.jp/かいけつゾロリのドラゴンたいじ-1-かいけつゾロリシリーズ-ポプラ社の小さな童話-ゆたか/dp/459102654X?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&psc=1&smid=AN1VRQENFRJN5', '小学生の時見てました', '2022-12-16 06:32:07');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
